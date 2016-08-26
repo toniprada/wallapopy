8fb820e258c33a181776f29c0f98dca69c5bcde7
Toni Prada <toniprada@gmail.com>
2016-08-26 17:59:06 +0200

    Do requests using requests lib

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wallapopy
 A Wallapop client for Python.
+
+**Work in progress**
--- client.py
+++ client.py
@@ -1,17 +1,25 @@
+import requests
+
 class WallapopClient:
 
-    BASE_URL = 'http://pro2.wallapop.com/shnm-portlet/api/v1'
+    BASE_URL = 'http://pro2.wallapop.com/shnm-portlet/api/v1/'
 
     def __init__(self):
         pass
 
-    def search(self, latitude, longitude):
-        print self._build_url('item.json/search8', ('longitude', latitude), ('latitude', longitude))
-        # TODO additional params for a effective search
-        # TODO do request
+    def search(self, latitude, longitude, order_by='creationDate', order_type='des',
+        time_filter='noLimit'):
+        params = {
+            'latitude': latitude,
+            'longitude': longitude,
+            'orderBy': order_by,
+            'orderType': order_type,
+            'timeFilter': time_filter
+        }
+        return self._get('item.json/search8', params)
+        # "&categoryIds=12545%2C12800%2C12579%2C12467%2C12900%2C12463%2C12465%2C12461%2C13000%2C13100%2C12485%2C13200
 
-    def _build_url(self, endpoint, *params):
-        params_string = '&'.join(['='.join([tup[0], str(tup[1])]) for tup in  params])
-        # TODO encode params
-        return '{0}/{1}?{2}'.format(self.BASE_URL, endpoint, params_string )
-
+    def _get(self, endpoint, params):
+        r = requests.get(self.BASE_URL + endpoint , params=params)
+        print r.url
+        return r.json()
