5c45cf9cdfe447f9dff943302f59897462170820
Toni Prada <toniprada@gmail.com>
2016-08-26 10:57:42 +0200

    Start working on search endpoint

--- client.py
+++ client.py
@@ -1,4 +1,17 @@
 class WallapopClient:
 
+    BASE_URL = 'http://pro2.wallapop.com/shnm-portlet/api/v1'
+
     def __init__(self):
-        print 'hello world'
+        pass
+
+    def search(self, latitude, longitude):
+        print self._build_url('item.json/search8', ('longitude', latitude), ('latitude', longitude))
+        # TODO additional params for a effective search
+        # TODO do request
+
+    def _build_url(self, endpoint, *params):
+        params_string = '&'.join(['='.join([tup[0], str(tup[1])]) for tup in  params])
+        # TODO encode params
+        return '{0}/{1}?{2}'.format(self.BASE_URL, endpoint, params_string )
+
---
+++ test.py
@@ -0,0 +1,4 @@
+from client import WallapopClient
+
+w = WallapopClient()
+w.search(40.41669, -3.700346)
