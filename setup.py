13530ed75ec120bc7eae10f9e33c9e6f12fd8cd4
Toni Prada <toniprada@gmail.com>
2017-02-22 13:30:28 +0100

    Increase package version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='wallapopy',
-    version='1.0.0',
+    version='1.0.1',
     description='A Wallapop client for Python.',
     long_description=long_description,
     url='https://github.com/toniprada/wallapopy',
